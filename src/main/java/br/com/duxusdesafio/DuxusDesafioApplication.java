1334a718a74c24906073e3bc4500aa8f79508754
VitorRodriigues <tdvse@hotmail.com>
2025-05-06 16:19:19 -0300

    Initial commit

---
+++ src/main/java/br/com/duxusdesafio/DuxusDesafioApplication.java
@@ -0,0 +1,13 @@
+package br.com.duxusdesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuxusDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuxusDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/duxusdesafio/DuxusDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.duxusdesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuxusDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
